7fbfd8eaeb4be70808afa895c06e6683d0d224a2
MasayaTamegai <MTamegai@gmail.com>
2017-01-16 16:41:01 -0600

    Bare Bones
    The beginnings

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<style>
+		ul {list-style-type: none;}
+		li {display: inline; margin-right: 20px; }	
+	</style>
+
+	<title>Home</title>
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="add_post.php">Add a Post</a></li>
+			<li><a href="add_category.php">Add a Category</a></li>
+			<li><a href="category_list.php">Category List</a></li>
+		</ul>
+	</nav>
+	<h1>www.MasayaTamegai.com</h1>
+	<p>Journey of Progress</p>
+</body>
+</html>
