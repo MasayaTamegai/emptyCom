eeba22ce6c6e3d216a833066ba2114846ac30c53
MasayaTamegai <MTamegai@gmail.com>
2017-01-21 17:56:25 -0600

    Merging a Branch
    Hopefully I do this right.

--- includes/db.php
+++ includes/db.php
@@ -19,3 +19,5 @@ $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 //}
 
 ?>
+
+ 
--- includes/navbar.php
+++
@@ -1,8 +0,0 @@
-	<nav>
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="add_post.php">Add a Post</a></li>
-			<li><a href="add_category.php">Add a Category</a></li>
-			<li><a href="category_list.php">Category List</a></li>
-		</ul>
-	</nav>
