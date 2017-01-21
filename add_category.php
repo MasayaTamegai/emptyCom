f8dbe9c19664e48aa7c54b7f81bf286546b72641
MasayaTamegai <MTamegai@gmail.com>
2017-01-20 18:00:57 -0600

    Got rid of files
    Deleted a few files and organized things

--- add_category.php
+++ add_category.php
@@ -14,19 +14,13 @@
 	<!-- Nav Bar -->
 	<?php include "includes/navbar.php" ?>
 	
-	<h1>Add a Category</h1>
-	<?php 
-	if (isset($error)) {
-		echo "<p> {$error} </p>\n";
-	}
-	?>
 	<form action="" method="post">
 		<div>
-			<label for="name"> Name </label>
+			<label for="cat_title"> Add a Category </label>
 			<input type="text" name="name" value="">
 		</div>
 		<div>
-			<input type="submit" value="Add Category">
+			<input type="submit" value="SUBMIT">
 		</div>
 	</form>
 </body>
