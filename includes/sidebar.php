15d44a8a4083d9f96a4aa5168746a090ba7632f7
MasayaTamegai <MTamegai@gmail.com>
2017-02-19 13:25:56 -0600

    login input
    added the login feature to the index

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -21,14 +21,15 @@
         
         <!-- Login -->
         <div class="well">
-            <h4>Blog Search</h4>
+            <h4>Login</h4>
             <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Enter Username">
+            </div>
             <div class="input-group">
-                <input name="search" type="text" class="form-control">
+                <input name="password" type="password" class="form-control" placeholder="Enter Password">
                 <span class="input-group-btn">
-                    <button name = "submit" class="btn btn-default" type="submit">
-                        <span class="glyphicon glyphicon-search"></span>
-                </button>
+                    <button class="btn btn-primary" name="login" type="submit">Submit</button>
                 </span>
             </div>
             </form> <!--SEARCH FORM-->
